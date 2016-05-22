70182e1f44d5ac88c6a8a89e558b2a2475ef874c
Kevin Chuang <kcxinkun@gmail.com>
2016-05-22 14:38:30 -0700

    corrected indentation and missing semicolons

--- test/test.js
+++ test/test.js
@@ -1,12 +1,14 @@
+/*eslint-env es6*/
+
 const chai = require('chai');
 const chaiHTTP = require('chai-http');
 const expect = chai.expect;
-chai.use(chaiHTTP)
-const request = chai.request
+chai.use(chaiHTTP);
+const request = chai.request;
 const Router = require('../lib/router.js');
 const router = new Router();
 
-require('../test_server.js')
+require('../test_server.js');
 
 describe('Testing GET request', () => {
   it('should give text on /pokemon', (done) => {
@@ -14,59 +16,59 @@ describe('Testing GET request', () => {
     .get('/pokemon')
     .end((err,res) => {
       expect(err).to.eql(null);
-      expect(res.text).to.eql('Here is a pikachu')
+      expect(res.text).to.eql('Here is a pikachu');
       done();
-    })
-  })
-})
+    });
+  });
+});
 
 describe('Testing POST request',()=> {
- it('should get text when sending a POST request',(done)=> {
-   request('localhost:3000')
+  it('should get text when sending a POST request',(done)=> {
+    request('localhost:3000')
    .post('/pokemon')
    .end((err,res) =>{
      expect(err).to.eql(null);
      expect(res).to.have.status(200);
      expect(res.text).to.eql('Added new pokemon');
      done();
-   })
- });
+   });
+  });
 });
 
 describe('Testing PUT request',()=> {
- it('should get text when sending a PUT request',(done)=> {
-   request('localhost:3000')
+  it('should get text when sending a PUT request',(done)=> {
+    request('localhost:3000')
    .put('/pokemon')
    .end((err,res) =>{
      expect(err).to.eql(null);
      expect(res).to.have.status(200);
      expect(res.text).to.eql('You updated pokemon list');
      done();
- });
-});
+   });
+  });
 });
 
 describe('Testing DELETE request',()=> {
- it('should get text when sending a DELETE request',(done)=> {
-   request('localhost:3000')
+  it('should get text when sending a DELETE request',(done)=> {
+    request('localhost:3000')
    .delete('/pokemon')
    .end((err,res) =>{
      expect(err).to.eql(null);
      expect(res).to.have.status(200);
      expect(res.text).to.eql('Deleted pokemon');
      done();
- });
-});
+   });
+  });
 });
 describe('Testing PATCH request', ()=> {
- it('should get text when sending a PATCH request',(done)=> {
-   request('localhost:3000')
+  it('should get text when sending a PATCH request',(done)=> {
+    request('localhost:3000')
    .patch('/pokemon')
    .end((err,res) =>{
      expect(err).to.eql(null);
      expect(res).to.have.status(200);
      expect(res.text).to.eql('You modified the pokemon list');
      done();
- });
-});
+   });
+  });
 });
--- test_Server.js
+++ test_Server.js
@@ -18,16 +18,16 @@ router.post('/pokemon', (req,res) => {
 });
 
 router.delete('/pokemon', (req,res) => {
-  res.send('Deleted pokemon')
-})
+  res.send('Deleted pokemon');
+});
 
 router.put('/pokemon', (req,res) => {
   res.send('You updated pokemon list');
-})
+});
 
 router.patch('/pokemon',(req,res) => {
   res.send('You modified the pokemon list');
-})
+});
 
 http.createServer(router.route())
-  .listen(3000, () => console.log('listening on 3000'))
+  .listen(3000, () => console.log('listening on 3000'));
